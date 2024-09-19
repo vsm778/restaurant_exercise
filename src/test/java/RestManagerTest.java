4ff9f7137943e3b6ff86d6812860da149447a37b
vsm778 <noemail@noemail.com>
2024-09-19 09:47:40 +0200

    updated tests

--- src/test/java/RestManagerTest.java
+++ src/test/java/RestManagerTest.java
@@ -72,6 +72,70 @@ class RestManagerTest {
         assertEquals(6, table.size);
     }
 
+    @Test
+    public void should_fill_all_tables_from_queue() {
+
+        UUID uuidGroup21 = UUID.randomUUID();
+        UUID uuidGroup31 = UUID.randomUUID();
+        UUID uuidGroup41 = UUID.randomUUID();
+        UUID uuidGroup51 = UUID.randomUUID();
+        UUID uuidGroup61 = UUID.randomUUID();
+
+        UUID uuidGroup22 = UUID.randomUUID();
+        UUID uuidGroup32 = UUID.randomUUID();
+
+        List<UUID> uuids = List.of(uuidGroup21, uuidGroup31, uuidGroup41, uuidGroup51, uuidGroup61);
+
+        restManager.onArrive(new ClientsGroup(2, uuidGroup21));
+        restManager.onArrive(new ClientsGroup(3, uuidGroup31));
+        restManager.onArrive(new ClientsGroup(4, uuidGroup41));
+        restManager.onArrive(new ClientsGroup(5, uuidGroup51));
+        restManager.onArrive(new ClientsGroup(6, uuidGroup61));
+
+        restManager.onArrive(new ClientsGroup(2, uuidGroup22));
+        restManager.onArrive(new ClientsGroup(3, uuidGroup32));
+
+        restManager.onLeave(new ClientsGroup(6, uuidGroup61));
+
+        Table table32 = restManager.lookup(new ClientsGroup(uuidGroup32));
+        Table table22 = restManager.lookup(new ClientsGroup(uuidGroup22));
+
+        assertEquals(table22, table32);
+
+    }
+
+    @Test
+    public void should_fill_all_tables_from_queue_3x3() {
+
+        UUID uuidGroup21 = UUID.randomUUID();
+        UUID uuidGroup31 = UUID.randomUUID();
+        UUID uuidGroup41 = UUID.randomUUID();
+        UUID uuidGroup51 = UUID.randomUUID();
+        UUID uuidGroup61 = UUID.randomUUID();
+
+        UUID uuidGroup32 = UUID.randomUUID();
+        UUID uuidGroup33 = UUID.randomUUID();
+
+
+
+        restManager.onArrive(new ClientsGroup(2, uuidGroup21));
+        restManager.onArrive(new ClientsGroup(3, uuidGroup31));
+        restManager.onArrive(new ClientsGroup(4, uuidGroup41));
+        restManager.onArrive(new ClientsGroup(5, uuidGroup51));
+        restManager.onArrive(new ClientsGroup(6, uuidGroup61));
+
+        restManager.onArrive(new ClientsGroup(2, uuidGroup32));
+        restManager.onArrive(new ClientsGroup(3, uuidGroup33));
+
+        restManager.onLeave(new ClientsGroup(6, uuidGroup61));
+
+        Table table32 = restManager.lookup(new ClientsGroup(uuidGroup32));
+        Table table33 = restManager.lookup(new ClientsGroup(uuidGroup33));
+
+        assertEquals(table32, table33);
+
+    }
+
 
     private void initRestManager(List<Table> tableList) {
         restManager = new RestManager(tableList);
